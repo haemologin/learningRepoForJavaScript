e4ddf4e9e1e8afcc6c1a055e7ab25f0ac8778bfe
Martin Varga <haemologin@gmail.com>
2018-03-09 16:46:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,54 +1,175 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>First one</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-</head>
-<body>
-    
-    <div class="container marginos">
-        <div class="jumbotron">
-            <h1 class="header1">Stratego</h1>
-            <p class="par1">My Stratego game project</p>
-        </div>
-    </div>
-    
-    <div class="container">
-        <h1 id="h1Game">The game itself</h1>
-        <div class="button-container">
-            <button class="buttons buttonNG">New game</button>
-            <button class="buttons buttonPU">Place units</button>
-            <button class="buttons buttonS">Start</button>
-        </div>
-        <!-- Board -->
-        <div class="row1">
+var table = document.querySelector(".row1");
+var boardElement;
+var placeUnitsButton = document.querySelector(".buttonPU");
+var element;
+var temp;
+var hiddenDiv = document.createElement("div");
+var isPlaceUnits = false;
+var previousElement;
+var xy;
+var availableUnits = document.querySelectorAll(".unitTypes");
+var unitPlacementSelected = false;
+var isPushed = false;
+var selectedUnit = 0;
+var fieldSelected = false;
+var counter = 0;
+var prevElem;
+var firstSelection = true;
+var unitName = "";
 
-        </div>   
-    </div>
-    
-    <div>
-        <div class="col-lg-3"></div>
-        <div class="unitDiv col-lg-6">
-            <ul>
-                <li>Bomb <span class="amount" id="bombAmount">0</span></li>
-                <li>Marshall <span class="amount" id="marshallAmount">0</span></li>
-                <li>General <span class="amount" id="generalAmount">0</span></li>
-                <li>Colonel <span class="amount" id="colonelAmount">0</span></li>
-                <li>Major <span class="amount" id="majorAmount">0</span></li>
-                <li>Captain <span class="amount" id="captainAmount">0</span></li>
-                <li>Lieutenant <span class="amount" id="lieutenantAmount">0</span></li>
-                <li>Sergeant <span class="amount" id="sergeantAmount">0</span></li>
-                <li>Miner <span class="amount" id="minerAmount">0</span></li>
-                <li>Spy <span class="amount" id="spyAmount">0</span></li>
-                <li>Flag <span class="amount" id="flagAmount">0</span></li>
-            </ul>
-        </div>
-        <div class="col-lg-3"></div>
-    </div>
+hiddenDiv.style.backgroundColor = "rgb(0,150,0)";
 
-    <script src="script.js"></script>
-</body>
-</html>
+//board - 10x10 size.
+var board = [[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0]];
+board[9][9] = 0;
+
+//Units for battle - first number is the strength, second is the amount of units can be placed on the board.
+var units = {
+    tomb:[
+    bomb = {strength:11,amount:6,name: "Bomb"},
+    marshall = {strength:10,amount:1,name: "Marshall"},
+    general = {strength:9,amount:1,name: "General"},
+    colonel = {strength:8,amount:2,name: "Colonel"},
+    major = {strength:7,amount:3,name: "Major"},
+    captain = {strength:6,amount:4,name: "Captain"},
+    lieutenant = {strength:5,amount:4,name: "Lieutenant"},
+    sergeant = {strength:4,amount:4,name: "Sergeant"},
+    miner = {strength:3,amount:5,name: "Miner"},
+    scout = {strength:2,amount:8,name: "Scout"},
+    spy = {strength:1,amount:1,name: "Spy"},
+    flag = {strength:-1,amount:1,name: "Flag"}
+    ]
+}
+
+var amounts = document.querySelectorAll(".amount");
+placeUnitsButton.addEventListener("click", function(){
+    placeUnits();
+    placeUnits2();
+});
+
+
+
+init();
+
+function init(){
+    for(i = 0; i<amounts.length; i++){
+        amounts[i].textContent = units.tomb[i].amount;
+    }
+    for(i = 0; i<10; i++){
+        for(k = 0; k<10; k++){
+            var elem = document.createElement("div");
+            elem.textContent = "0";
+            elem.className = "col-xs-1-10";
+            board[i][k]=elem;
+            table.appendChild(elem);
+        }
+    }
+    element = document.querySelectorAll(".col-xs-1-10");
+    for(i = 60; i<element.length; i++){
+        temp = element[i];
+        previousElement = element[60];
+        temp.addEventListener("click", function(){
+            if(isPlaceUnits && this.style.backgroundColor !== hiddenDiv.style.backgroundColor){
+                fieldSelected = true;
+                if(!unitPlacementSelected){
+                    if(!firstSelection){
+                        if(Number(amounts[counter].textContent) > 0 && this.textContent != selectedUnitStrength){
+                            if(this.textContent != 0){
+                                var prevText = this.textContent;
+                                this.textContent = selectedUnitStrength;
+                                for(n = 0; n<units.tomb.length; n++){
+                                    if(prevText == units.tomb[n].strength){
+                                        amounts[n].textContent = Number(amounts[n].textContent) + 1;
+                                        console.log(n); 
+                                    }
+                                }
+                            }
+                            this.textContent = selectedUnitStrength;
+                            this.style.backgroundColor = "blue";
+                            amounts[counter].textContent = Number(amounts[counter].textContent) -1;
+                        }
+                    }
+                }
+                 
+                if(previousElement !== this){
+                    previousElement.style.backgroundColor = "#42f471";
+                    previousElement = this;
+                    xy = Array.prototype.indexOf.call(element, this);
+                    console.log(xy);
+                }
+                this.style.backgroundColor = "rgb(0,150,0)"; 
+            }else{
+                if(this.style.backgroundColor === hiddenDiv.style.backgroundColor && isPlaceUnits){
+                    this.style.backgroundColor = "#42f471";
+                    fieldSelected = false;
+                } 
+            }
+        });
+    }  
+}
+
+boardElementSelector(6,4);
+
+function boardElementSelector(x, y){
+    var element = board[x][y];
+    var elementIndex = (x*10) + y;
+    boardElement = document.querySelectorAll(".col-xs-1-10")[elementIndex]; 
+}
+
+function placeUnits(){
+    isPlaceUnits = true;
+    for(i = 0; i<10; i++){
+        for(k = 0; k<10; k++){
+            if(i>=6){
+                board[i][k].style.backgroundColor = "#42f471";
+            }
+        }
+    }
+}
+
+
+function placeUnits2(){
+    if(!isPushed){
+        
+        var temp;
+        var tempdiv = document.createElement("div");
+        prevElem = availableUnits[0];
+        tempdiv.style.color = "white";
+        for(i = 0; i < availableUnits.length; i++){
+            temp = availableUnits[i];
+            temp.addEventListener("click", function(){
+                if(tempdiv.style.color !== this.style.color){
+                    firstSelection = false;
+                    this.style.backgroundColor = "green";
+                    this.style.color = "white";
+                    counter = Array.prototype.indexOf.call(availableUnits, this);
+                    selectedUnit = Number(amounts[counter].textContent);
+                    for(j = 0; j<units.tomb.length; j++){
+                        unitName = availableUnits[counter].textContent;
+                        unitName = unitName.substring(0,unitName.length-2);
+                        if(unitName === units.tomb[j].name){
+                            selectedUnitStrength = units.tomb[j].strength;
+                        }
+                    }
+
+                    if(prevElem !== this){
+                        prevElem.style.backgroundColor = "white";
+                        prevElem.style.color = "black";
+                        prevElem = this;
+                    }
+                    unitPlacementSelected = true;
+                }else {
+                    this.style.backgroundColor = "white";
+                    this.style.color = "black";
+                    unitPlacementSelected = false;
+                    selectedUnit = 0;
+                }
+                unitPlacementSelected = false;
+            });
+        }
+        isPushed = true;
+    }
+}
+
+
+
